e7c52f0d1c5201b9f3953d635ec05e829fc51f21
Gonzalo Nunez <gonzonv@gmail.com>
2019-04-24 06:31:16 -0500

    Start of project with views

---
+++ Maqueta/src/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>IQOS Care Plus</title>
+	<meta name="description" content="">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="./lib/css/normalize.css">
+	<link rel="stylesheet" href="./lib/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./styles/main.css">
+	<link rel="stylesheet" href="./lib/css/jquery-ui.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+	
+	<div class="wrapper">
+		
+	</div>
+	
+
+	
+
+	<script src="./lib/js/jquery-3.3.1.min.js"></script>
+	<script src="./lib/js/jquery-ui.min.js"></script>
+	<script src="./lib/js/bootstrap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+	<script src="./lib/js/slick.js"></script>
+	<script src="./js/general.js"></script>
+</body>
+</html>
